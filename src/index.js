b5b761de69f72d73e82807eae4c4062d5f5696df
ArthurRFr <arthur_fronza2011@hotmail.com>
2021-04-27 21:49:07 -0300

    Middlewares - Finished

--- src/index.js
+++ src/index.js
@@ -10,19 +10,67 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return  response.status(404).json({ error: "User not found." });
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if( user.pro === true ) {
+    return next();
+  } else {
+    if(user.todos.length < 10) {
+      return next();
+    } else {
+      return response.status(403).json({ error: "You must subscribe to the pro version to add a todo." });
+    }
+  }
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+  
+  if(!user) {
+    return response.status(400).json({ error: "User doesn't exist." });
+  }
+  
+  const todo = user.todos.find(todo => todo.id === id);
+
+  if(!todo) {
+    return response.status(404).json({ error: "Todo not found." });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user) {
+    return  response.status(404).json({ error: "User '" + id + "' not found." })
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
